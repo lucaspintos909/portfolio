5b0561bb79772e8cea31b7d71789621b95950a4a
lucaspintos909 <lucaspintos909@gmail.com>
2021-11-29 23:33:49 -0300

    refactor(js): QualificationTabs indentado correctamente

--- js/modules/qualificationTabs.js
+++ js/modules/qualificationTabs.js
@@ -6,15 +6,15 @@ tabs.forEach((tab) => {
     const target = document.querySelector(tab.dataset.target);
 
     tabContents.forEach((tabContent) => {
-      tabContent.classList.remove('cualification__active')
+      tabContent.classList.remove("qualification__active");
     });
 
-    target.classList.add('cualification__active');
+    target.classList.add("qualification__active");
 
-    tabs.forEach(tab => {
-      tab.classList.remove('cualification__active');
-    })
+    tabs.forEach((tab__) => {
+      tab__.classList.remove("qualification__active");
+    });
 
-    tab.classList.add('cualification__active');
+    tab.classList.add("qualification__active");
   });
 });
