c7465fa2b0ca06252864b7e00ba43dc992a3d89c
lucaspintos909 <lucaspintos909@gmail.com>
2021-11-29 22:37:47 -0300

    feat(main.js): Añadiendo modulo qualificationTabs

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
-const modules = ["modules/navbar", "modules/skillsAccordion"];
+const modules = [
+  "modules/navbar",
+  "modules/skillsAccordion",
+  "modules/qualificationTabs",
+];
 requirejs(modules, function () {});
